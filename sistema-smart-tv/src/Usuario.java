df40f8f649b1330467aba8afa727d5e0c5a7cee5
guilh <guilherme.souza.1997@outlook.com>
2023-04-23 17:51:50 -0300

    repositórios do projeto AboutMe para exercitar no terminal

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,19 +1,20 @@
-public class Usuario {
+    public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
-        System.out.println(" Canal atual :" +smartTv.canal); 
+
+            System.out.println(" Canal atual :" +smartTv.canal); 
         smartTv.mudarCanal(13);
-        System.out.println(" Canal atual :" +smartTv.canal);
+            System.out.println(" Canal atual :" +smartTv.canal);
 
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.aumentarVolume();
-        System.out.println(" Volume atual :" +smartTv.volume);
+            System.out.println(" Volume atual :" +smartTv.volume);
 
-        System.out.println(" TV Ligada ?" + smartTv.ligada);
-        System.out.println(" Canal atual :" +smartTv.canal);
-        System.out.println(" Volume atual :" +smartTv.volume);
+            System.out.println(" TV Ligada ?" + smartTv.ligada);
+            System.out.println(" Canal atual :" +smartTv.canal);
+            System.out.println(" Volume atual :" +smartTv.volume);
 
 
 
---
+++ sistema-smart-tv/src/minhaClasse.java
@@ -0,0 +1,8 @@
+public class minhaClasse {
+
+    public static void main(String[] args) {
+        System.out.println("oi turma, fui execultado pelo terminal");
+    }
+    
+    
+}
